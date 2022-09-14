3026ec3518824484d95f6cd7b99f9f52a82e8045
TrevorWinstral <trevor.winstral@gmail.com>
2022-09-14 05:29:29 +0200

    cleaned up and added html output

--- elo_param_optimizations.py
+++ elo_param_optimizations.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-CURRENT_SEASON='2022'
-
 df = pd.read_csv('results.csv', index_col=0)
 weeks = ['0']
 weeks.extend(list(df['Week'].unique()))
@@ -11,11 +9,6 @@ team_ind = {teams[i]:i for i in range(len(teams))}
 base_elo = 0
 realization_delta = pd.DataFrame(index=teams)
 reset = lambda factor, level: factor*(level- base_elo) + base_elo # for bracket reset, see more below
-
-#k_opt, l_opt, r_opt=11.0, 200.0, 0.70
-logistic_factor = 200 # 400 for chess, the larger the more likely upsets are, so upsetters are rewarded less
-reset_factor = 0.7 #after season do factor * (elo-base_elo) + base_elo, 0 is hard reset, 1 is no reset
-baseK = 11 #maximal change of elo per game (this maybe should adjust throughout the season, probably declining)
 
 def score_realization_diff(row, expected_score_matrix=None):
     #assert expected_score_matrix != None
@@ -60,11 +53,76 @@ def elo_sim(baseK, logistic_factor, reset_factor):
         K -= 0 #1/3
     return elo
 
+logistic_factor = 100 # 400 for chess, the larger the more likely upsets are, so upsetters are rewarded less
+reset_factor = 0.65 #after season do factor * (elo-base_elo) + base_elo, 0 is hard reset, 1 is no reset
+baseK = 40 #maximal change of elo per game (this maybe should adjust throughout the season, probably declining)
 elo = elo_sim(baseK, logistic_factor, reset_factor)
+
 
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+
+# We have 3 main arbitrary parameters, logistic factor, K (and the amount that we change K by), and the reset factor
+# we want to minimize MSE between between expected score and realized score. We could just find min over all combinations of these
+actual_score_matrix = np.zeros((32,32))
+def apply_scores(row):
+    t1, t2 = row[['Winner/tie', 'Loser/tie']]
+    t1i, t2i = team_ind[t1], team_ind[t2]
+    if row['isTie']:
+        actual_score_matrix[t1i, t2i] = 0.5
+        actual_score_matrix[t2i, t1i] = 0.5
+    else:
+        actual_score_matrix[t1i, t2i] = 1
+        actual_score_matrix[t2i, t1i] = 0
+
+def elo_MSE(elo, results):
+    global actual_score_matrix
+    SSE = 0
+
+    for w_ind in range(1, len(weeks)):
+        realization_delta['delta'] = 0
+        w = weeks[w_ind] # current week
+        last_w = weeks[w_ind-1] # last week
+        g = results[results['Week']==w] # games for week w
+        #calculate expected score for winner
+        ones = np.array([1]*32)
+        if w[:4] != last_w[:4] and last_w[0] == '2':
+            last_w = 'Reset Week ' + w[:4] # last week set to reset week
+        elo_diffs = np.outer(elo[last_w],ones) - np.outer(elo[last_w],ones).T
+        expected_score_matrix = 1/(1 + 10**(-1*elo_diffs/logistic_factor))# expected score for i playing against j given by [i,j]'th entry
+        actual_score_matrix = np.copy(expected_score_matrix)
+        _ = g.apply(apply_scores, axis=1)
+        scores_half = np.triu(actual_score_matrix - expected_score_matrix, 1)**2 # we don't need to count games twice
+        SSE += np.sum(scores_half)
+
+    return SSE/(len(weeks)-1)
+
+KN, LN, RN =  31, 16, 21 #31, 16, 21
+K_range = np.linspace(10,40,KN)
+logistic_range = np.linspace(50, 800, LN)
+reset_range = np.linspace(0, 1, RN)
+
+from tqdm.auto import tqdm
+results = np.zeros((KN, LN, RN))
+for k in tqdm(range(len(K_range))):
+    for l in tqdm(range(len(logistic_range))):
+        for r in range(len(reset_range)):
+            e = elo_sim(K_range[k],logistic_range[l],reset_range[r])
+            results[k,l,r] = elo_MSE(e, df)
+
+flat_ind=np.argmin(results)
+unflat_ind = np.unravel_index(flat_ind, results.shape)
+k_opt = K_range[unflat_ind[0]]
+l_opt = logistic_range[unflat_ind[1]]
+r_opt = reset_range[unflat_ind[2]]
+print(k_opt, l_opt, r_opt) # 11.0, 200.0, 0.7000000000000001
+#k_opt, l_opt, r_opt=11.0, 200.0, 0.70
+
+plt.hist(results.flatten())
+plt.show()
+
+elo = elo_sim(k_opt, l_opt, r_opt)
 f = plt.figure()
 for i in range(16):
     _=elo.loc[teams[i]].plot(label=teams[i])
@@ -89,38 +147,5 @@ for i in top12:
 
 f.set_figwidth(5)
 plt.axvline(x=21, color='black', linestyle='--')
-plt.axvline(x=42, color='black', linestyle='--')
 f.legend(loc='upper left')
 f.show()
-
-
-gs = pd.read_csv('next_games.csv')[['Winner/tie', 'Loser/tie']]
-def predict(row, elos):
-    t1 = row['Winner/tie']
-    t2 = row['Loser/tie']
-
-    elo_diff = elos[t1] - elos[t2]
-    expected_score = 1/(1 + 10**(-1*elo_diff/logistic_factor))
-
-    return expected_score
-    
-
-elos = elo.iloc[:,-1]
-gs['Prediction'] = gs.apply(predict, args=(elos,), axis=1)
-gs.rename(columns={'Winner/tie':'Team 1', 'Loser/tie':'Team 2'}, inplace=True)
-gs
-print(gs.to_html(index=False))
-
-
-# create current power rankings
-print(pd.DataFrame(np.array(best_teams.index).reshape((8,4))).to_html(index=False, header=False))
-
-# power rankings for this season
-current_season = [w for w in elo.columns if w[:4]==CURRENT_SEASON or w==f'Reset Week {CURRENT_SEASON}']
-current_season.reverse()
-rankings = pd.DataFrame([elo[w].sort_values(ascending=False).index for w in current_season]).T
-new_colnames=['Week 0']
-new_colnames.extend([f'Week {w[5:]}' for w in elo.columns if w[:4]==CURRENT_SEASON])
-new_colnames.reverse()
-rankings.columns= new_colnames
-print(rankings.to_html())
--- process_data.py
+++ process_data.py
@@ -1,13 +1,21 @@
 import pandas as pd
-
+# data from https://www.pro-football-reference.com/years/2021/games.htm
 df20 = pd.read_csv('2020.csv')
 df20['isTie'] = df20['PtsW']==df20['PtsL']
 df20['Week'] = '2020-'+df20['Week']
 
 df21 = pd.read_csv('2021.csv')
 df21['isTie'] = df21['PtsW']==df21['PtsL']
-df20['Week'] = '2021-'+df21['Week']
+df21['Week'] = '2021-'+df21['Week']
 
-df = pd.concat([df20, df21]).reset_index()
-df = df[['Week', 'Date', 'Winner/tie', 'Loser/tie', 'isTie', 'PtsW', 'PtsL']]
-df.to_csv('results.csv')
+df22 = pd.read_csv('2022.csv')
+df22['isTie'] = df22['PtsW']==df22['PtsL']
+df22['Week'] = '2022-'+df22['Week'].astype(str)
+
+df = pd.concat([df20, df21, df22]).reset_index()
+df = df[['Week', 'Date', 'Winner/tie', 'Loser/tie', 'isTie', 'PtsW', 'PtsL', 'type']]
+df[df['type']=='boxscore'].to_csv('results.csv')
+
+#get next weeks games
+next_week=df[df['type']=='preview'].iloc[0]['Week']
+df[df['Week'] == next_week].to_csv('next_games.csv')
